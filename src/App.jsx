ad9426f32f7eab9488bc866c66acdeb68efc61e2
Prajjwal1203 <pmprajjwal12@gmail.com>
2024-10-11 20:33:39 +0530

    New Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,47 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import AppLayout from './layouts/app-layout'
-import LandingPage from './pages/landing'
-import Dashboard from './pages/dashboard'
-import Auth from './pages/auth'
-import Link from './pages/link'
-import RedirectLink from './pages/redirect-link'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/landing";
+import Dashboard from "./pages/dashboard";
+import Auth from "./pages/auth";
+import Link from "./pages/link";
+import RedirectLink from "./pages/redirect-link";
+import UrlProvider from "./context";
 
 const router = createBrowserRouter([
   {
-    element:<AppLayout/>,
-    children:[
+    element: <AppLayout />,
+    children: [
       {
-        path:'/',
-        element:<LandingPage></LandingPage>,
+        path: "/",
+        element: <LandingPage></LandingPage>,
       },
       {
-        path:'/dashboard',
-        element:<Dashboard></Dashboard>,
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>,
       },
       {
-        path:'/auth',
-        element:<Auth></Auth>,
+        path: "/auth",
+        element: <Auth></Auth>,
       },
       {
-        path:'/link/:id',
-        element:<Link></Link>,
+        path: "/link/:id",
+        element: <Link></Link>,
       },
       {
-        path:'/:id',
-        element:<RedirectLink></RedirectLink>,
+        path: "/:id",
+        element: <RedirectLink></RedirectLink>,
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 
 function App() {
   return (
-    <RouterProvider router={router}>
+    <UrlProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </UrlProvider>
+  );
+}
 
-    </RouterProvider>
-  )}
-
-export default App
+export default App;
--- src/_components/login.jsx
+++ src/_components/login.jsx
@@ -10,88 +10,102 @@ import {
 import { Input } from "@/components/ui/input";
 import { BeatLoader } from "react-spinners";
 import Error from "./error";
-import { useState } from "react";
-import * as Yup from "yup"
+import { useState, useEffect } from "react";
+import * as Yup from "yup";
+import useFetch from "@/hooks/use-fetch";
+import { login } from "@/db/apiAuth";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { UrlState } from "@/context";
 
 const Login = () => {
-  const[errors,setErrors] = useState([])
-  const[fromData,setFormData] = useState({
-    email:"",
-    password:"",
+  const [errors, setErrors] = useState([]);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
   });
+  const navigate = useNavigate();
+  let [searchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew");
 
-
-  const handleInputChange = (e) =>
-  {
-    const{name,value} =e.target;
-    setFormData((prevState) =>({
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
       ...prevState,
-      [name]:value,
-    }))
+      [name]: value,
+    }));
   };
 
-  const handleLogin = async () =>
-  {
-    setErrors([])
-    try
+  const { data, error, loading, fn: fnLogin } = useFetch(login, formData);
+
+  const{fetchUser} = UrlState()
+
+  useEffect(() =>{
+    if(error === null && data)
     {
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}`:""}`)
+      fetchUser();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data, error]);
+
+  const handleLogin = async () => {
+    setErrors([]);
+    try {
       const schema = Yup.object().shape({
         email: Yup.string()
-        .email("Invalid Email")
-        .required("email is required"),
+          .email("Invalid Email")
+          .required("email is required"),
 
         password: Yup.string()
-        .min(6,"Password must be at least of 6 characters")
-        .required("Password is required")
+          .min(6, "Password must be at least of 6 characters")
+          .required("Password is required"),
       });
 
-      await schema.validate(fromData,{abortEarly:false});
-
-    }
-    catch(e)
-    {
+      await schema.validate(formData, { abortEarly: false });
+      await fnLogin();
+    } catch (e) {
       const newErrors = {};
 
-      e?.inner?.forEach((err) =>{
+      e?.inner?.forEach((err) => {
         newErrors[err.path] = err.message;
       });
       setErrors(newErrors);
     }
-
-  }
+  };
   return (
-  
-      <Card>
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>To your account if you have one</CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-2">
-          <div className="space-y-1">
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>To your account if you have one</CardDescription>
+        {error && <Error message={error.message} />}
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
           <Input
-           name="email"
-           type="email" 
-           placeholder="Enter Email"
-           onChange={handleInputChange}/>
-          {errors.email && <Error message={errors.email} ></Error>} 
-          </div>
+            name="email"
+            type="email"
+            placeholder="Enter Email"
+            onChange={handleInputChange}
+          />
+          {errors.email && <Error message={errors.email}></Error>}
+        </div>
 
-          <div className="space-y-1">
-          <Input 
-          name="password" 
-          type="password" 
-          placeholder="Enter Password" 
-          onChange={handleInputChange}/>
-          {errors.password && <Error message={errors.password} ></Error>} 
-          </div>
-        </CardContent>
-        <CardFooter>
+        <div className="space-y-1">
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter Password"
+            onChange={handleInputChange}
+          />
+          {errors.password && <Error message={errors.password}></Error>}
+        </div>
+      </CardContent>
+      <CardFooter>
         <Button onClick={handleLogin}>
-          {true?<BeatLoader size={10} color="#36d7b7"/> : "Login"}
-          </Button>
-        </CardFooter>
-      </Card>
-    
+          {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
